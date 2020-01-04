9794ba2d3f38587f1b146c8b0e9177008efdb928
BiggMiike <38381190+BiggMiike@users.noreply.github.com>
2020-01-04 11:35:32 +0000

    Piloto Surpresa

--- TP_POO/PilotoSurpresa.cpp
+++ TP_POO/PilotoSurpresa.cpp
@@ -1,4 +1,7 @@
 #include "PilotoSurpresa.h"
+#include "Pista.h"
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
@@ -19,7 +22,41 @@ std::string PilotoSurpresa::obtemTipo() const
 
 bool PilotoSurpresa::tomaDecisao(Carro * c, Pista * p)
 {
-	return false;
+	srand((int)time(NULL));
+	int prob = rand() % 100;
+	int posAtual = p->obtemPosCorrida(c->obtemId());
+	int carrosNaPista = p->obtemCarrosNaPista();
+
+	if (prob < 10) {//faz algo que danifica o automovel
+		c->danificaCarro();
+		cout << c->obtemNomePiloto() << " danificou o carro " << c->obtemId() << endl;	
+		if (p->obtemPosCorrida(c->obtemId()) + 1 < p->obtemNumCarrosEmCompeticao() && p->obtemPosCorrida(c->obtemId()) - 1 > 1) {  //Se nao estiver em ultimo, danifica o ultimo, nem em primeiro
+			p->danificaCarro(posAtual + 1); //Danifica o antes
+			p->danificaCarro(posAtual - 1); //Danifica o carro a frente
+			cout << "ESTA NO MEIO";
+			return true;
+		}
+		else if (p->obtemPosCorrida(c->obtemId()) == 1) {
+			p->danificaCarro(posAtual + 1); //Danifica o carro atrás
+			cout << "ESTA em primeiro";
+			return true;
+		}
+		else if (p->obtemPosCorrida(c->obtemId()) == p->obtemNumCarrosEmCompeticao()) { // Se estiver em ultimo
+			p->danificaCarro(posAtual - 1);
+			cout << "ESTA em ultimo";
+			return true;
+		}
+		/*else {
+			p->danificaCarro(posAtual);
+			cout << "NEM UM NEM OUTRO";
+			return true;
+		}*/
+	}
+	else {
+		c->acelera();
+	}
+
+	return true;
 }
 
 Piloto * PilotoSurpresa::dupilica()
